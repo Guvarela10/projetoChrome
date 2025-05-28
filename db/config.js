6b679e3c8a45ab66169afbb70532e327e2e307cb
Guvarela10 <guvarella945@gmail.com>
2025-05-27 22:45:13 -0300

    atualizadooo

--- db/config.js
+++ db/config.js
@@ -1,13 +1,17 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
 export default {
     development: {
         dialect: 'sqlite',
         storage: 'database.db'
     },
-    // production: {
-    //     dialect: 'mysql',
-    //     username: 'root',
-    //     password: '050802100',
-    //     database: 'projetoChrome',
-    //     host: '127.0.0.1'
-    // }
+    production: {
+        dialect: process.env.DB_DIALECT,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST
+    }
 } 
--- db/database.js
+++ db/database.js
@@ -2,7 +2,7 @@ import { Sequelize } from 'sequelize'
 import config from './config.js'
 
 // CRIANDO BANCO DE DADOS
-const database = new Sequelize(config.development)
+const database = new Sequelize(config[process.env.NODE_ENV || 'development'])
 
 // TRY CATCH PARA INICIAR O BANCO DE DADOS
 try {
